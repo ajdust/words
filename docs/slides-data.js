bbaa52a478aceb14055f7220041e168995fa6794
Aaron Johnson <johnsoaab@gmail.com>
2025-12-11 21:21:27 -0800

    Update slides

---
+++ docs/slides-data.js
@@ -0,0 +1,26 @@
+window.slides = [
+  {
+    imageUrl: "Images/EthylContainsLead.webp",
+    title: "Ethyl Leaded Gasoline"
+  },
+  {
+    imageUrl: "Images/EthylGrannies1954.jpg",
+    title: "Ethyl Grannies"
+  },
+  {
+    imageUrl: "Images/Toxic-effects-lead-WHO-logo.jpg",
+    title: "Lead is toxic"
+  },
+  {
+    imageUrl: "Images/LeadedGasBannedChart.jpg",
+    title: "Leaded gas for cars banned!"
+  },
+  {
+    imageUrl: "Images/FirstSocialBan2025.png",
+    title: "Australia bans underage social media"
+  },
+  {
+    imageUrl: "Images/MoreDoctorsSmokeCamels.webp",
+    title: "Doctors smoke Camels 🚬"
+  }
+];
